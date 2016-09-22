6d5dd7cc489302b334a13084089c28e87ac50603
Simone Taeggi <startae14@gmail.com>
2016-09-22 12:26:26 +0200

    aggiunto il lightbox

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ $("nav").append($button);
 $button.click(function(){
 	window.location =$select.val();
 })
+
+
+$("#gallery li img").click(function(){
+	event.preventDefault();
+	var src = $(this).attr('src');
+	$($overlay).show();
+	$($immagine).attr('src', src);
+	$($immagine).attr('alt', $(this).attr('alt'));
+	$($immagine).show();
+	var testoTitolo = $(this).next().text();
+	$titolo.text(testoTitolo);
+});
+
+// creo overlay
+var $overlay = $('<div id="overlay"></div>');
+var $immagine = $('<img>');
+var $titolo = $('<p></p>');
+
+$('body').append($overlay);
+$overlay.append($immagine);
+$overlay.append($titolo);
+
+$overlay.click(function(){
+	$(this).hide();
+});
