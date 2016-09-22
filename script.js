40a37e4c849e75178aaf20f2700a17e58539aa68
Simone Taeggi <startae14@gmail.com>
2016-09-22 18:02:22 +0200

    aggiunta animazione nel mio nome

--- script.js
+++ script.js
@@ -42,3 +42,13 @@ $overlay.append($titolo);
 $overlay.click(function(){
 	$(this).hide();
 });
+
+
+var terms = ["Web Designer", "Frontend Dev", "Illustrator", " Trekker", " Traveler"];
+
+function rotateTerm() {
+  var ct = $("#rotate").data("term") || 0;
+  $("#rotate").data("term", ct == terms.length -1 ? 0 : ct + 1).text(terms[ct]).fadeIn()
+              .delay(2000).fadeOut(400, rotateTerm);
+}
+$(rotateTerm);
